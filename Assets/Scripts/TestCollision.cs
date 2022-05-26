33373fe453a6e6c3f54bf309096e72eda3aa3cb9
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-05-26 13:49:08 -0300

    Collisions started

---
+++ Assets/Scripts/TestCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+    }
+
+    void OnCollisionExit()
+    {
+        Debug.Log("No contacto");
+    }
+
+    void OnCollisionStay()
+    {
+        Debug.Log("Está en contacto");
+    }
+}
